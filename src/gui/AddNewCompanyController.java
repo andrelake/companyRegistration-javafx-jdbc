c33cb0f6530d60410c7bd7373a8b964c6900d9ab
andrelake <andre.lake@hotmail.com>
2020-03-08 17:27:54 -0300

    BasicInfo and CompanyInfo update

--- src/gui/AddNewCompanyController.java
+++ src/gui/AddNewCompanyController.java
@@ -9,40 +9,51 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 
 public class AddNewCompanyController implements Initializable{
 
-	ObservableList<String> typeList = FXCollections.observableArrayList("Public", "Private");
+	ObservableList<String> typeList = FXCollections.observableArrayList("Semiannual", "Yearly");
+	ObservableList<String> legalStatusList = FXCollections.observableArrayList("Active", "Inactive");
 	
+	// Basic Info
 	@FXML
 	private TextField txtFieldName;
-	
 	@FXML
 	private TextField txtFieldAdress;
-	
 	@FXML
 	private TextField txtFieldCity;
-	
 	@FXML
 	private TextField txtFieldPhone;
-	
 	@FXML
 	private TextField txtFieldEmail;
-	
 	@FXML
 	private DatePicker datePickerContract;
-	
 	@FXML
 	private TextField txtFieldDuration;
-	
 	@FXML
 	private ChoiceBox<String> choiceBoxType;
 	
+	// Company info
+	@FXML
+	private TextField txtFieldId;
+	@FXML
+	private TextField txtFieldFantasyName;
+	@FXML
+	private TextField txtFieldMainCategory;
+	@FXML
+	private TextField txtFieldNationalId;
+	@FXML
+	private ComboBox<String> comboBoxLegalStatus;
+	@FXML
+	private TextField txtFieldTotalEmployees;
+	@FXML
+	private TextField txtFieldAdministratorName;
+	
 	@FXML
 	private Button btnOk;
-	
 	@FXML
 	private Button btnCancel;
 	
@@ -59,5 +70,6 @@ public class AddNewCompanyController implements Initializable{
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		choiceBoxType.setItems(typeList);
+		comboBoxLegalStatus.setItems(legalStatusList);
 	}
 }
